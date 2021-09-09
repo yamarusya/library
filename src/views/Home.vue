e5a1988523fc922fa0721383c07c5d3691f5af77
Марина Бондаренко <mvbondarenko_2@miem.hse.ru>
2021-09-09 10:13:56 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <input placeholder="book">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
